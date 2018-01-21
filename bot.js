88792c111afc87582c8a44de9fe3bda5c1aad8c8
deadyui <35664551+deadyui@users.noreply.github.com>
2018-01-21 13:48:23 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,12 +21,21 @@ client.on('message', message => {
     if (message.content === '~help') {
     	message.reply('**Here is a list of my current commands:**\n\n~ping\n~om\n~yum\n~kill me\n~help');
   	}
-    if (message.startsWith === '~userinfo') {
-    	message.reply('**User Info:**\n\n');
+    if (message.content === '~ping roulette') {
+    	message.reply('@here ouch.. try again?');
     }
-    if (message.content === '~userinfo') {
-    	message.reply('**Here is a list of my current commands:**\n\n~ping\n~om\n~yum\n~kill me\n~help');
-    }
+    if (message.content === '~murder @yui#9194') {
+    	message.reply('Executing [10-ping murder]');
+        message.reply('@yui#9194 will be murdered in 9 pings');
+        message.reply('@yui#9194 will be murdered in 8 pings');
+        message.reply('@yui#9194 will be murdered in 7 pings');
+        message.reply('@yui#9194 will be murdered in 6 pings');
+        message.reply('@yui#9194 will be murdered in 5 pings');
+        message.reply('@yui#9194 will be murdered in 4 pings');
+        message.reply('@yui#9194 will be murdered in 3 pings');
+        message.reply('@yui#9194 will be murdered in 2 pings');
+        message.reply('@yui#9194 will be murdered in 1 ping');
+        message.reply('@yui#9194 has been murdered.');
 });
 
 client.login(process.env.BOT_TOKEN);
