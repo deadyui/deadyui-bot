b727ea3ba07be3704f46ba8473cb2ac140a1500e
deadyui <35664551+deadyui@users.noreply.github.com>
2018-01-21 18:29:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,14 @@ client.on('message', message => {
     if (message.content === '~om') { 
         message.reply('nom nom');
     }
-    
+    if(command === "purge") {
+    const deleteCount = parseInt(args[0], 10);
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    const fetched = await message.channel.fetchMessages({count: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
     if (message.content === '~run it down mid') {
          message.reply('T1T1T1T1T1T1T1T1T1T1');
      }
