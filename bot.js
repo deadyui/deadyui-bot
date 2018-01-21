2b6a8d719ed4c19c78e61b3178728dc87495ff32
deadyui <35664551+deadyui@users.noreply.github.com>
2018-01-21 14:01:59 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,18 +24,18 @@ client.on('message', message => {
     if (message.content === '~ping roulette') {
     	message.reply('@here ouch.. try again?');
     }
-    if (message.content === '~murder @yui') {
+    if (message.content === '~murder me') {
     	message.reply('Executing [10-ping murder]');
-        message.reply('@yui#9194 will be murdered in 9 pings');
-        message.reply('@yui#9194 will be murdered in 8 pings');
-        message.reply('@yui#9194 will be murdered in 7 pings');
-        message.reply('@yui#9194 will be murdered in 6 pings');
-        message.reply('@yui#9194 will be murdered in 5 pings');
-        message.reply('@yui#9194 will be murdered in 4 pings');
-        message.reply('@yui#9194 will be murdered in 3 pings');
-        message.reply('@yui#9194 will be murdered in 2 pings');
-        message.reply('@yui#9194 will be murdered in 1 ping');
-        message.reply('@yui#9194 has been murdered.');
+        message.reply('you will be murdered in 9 pings');
+        message.reply('you will be murdered in 8 pings');
+        message.reply('you will be murdered in 7 pings');
+        message.reply('you will be murdered in 6 pings');
+        message.reply('you will be murdered in 5 pings');
+        message.reply('you will be murdered in 4 pings');
+        message.reply('you will be murdered in 3 pings');
+        message.reply('you will be murdered in 2 pings');
+        message.reply('you will be murdered in 1 ping');
+        message.reply('you has been murdered.');
     }
 });
 
