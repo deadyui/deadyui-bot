2936941a6e4613e22b4c652758941e50d6a5b5c7
deadyui <35664551+deadyui@users.noreply.github.com>
2018-01-21 18:21:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,13 +6,16 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '~om') {
+    if (message.content === '~om') { 
         message.reply('nom nom');
     }
     if(command === "ping") {
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
+    if (message.content === '~run it down mid') {
+         message.reply('T1T1T1T1T1T1T1T1T1T1');
+     }
 });
 
 client.login(process.env.BOT_TOKEN);
