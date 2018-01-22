edf6c50b83887362ae3d147d8c12910c81939937
deadyui <35664551+deadyui@users.noreply.github.com>
2018-01-21 20:11:30 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,11 +9,8 @@ client.on('message', message => {
     if (message.content === '+om') { 
         message.reply('nom nom');
     }
-    if (message.content === '+members') {
-        message.reply('There are currently **18** members in the server');
-    }
     if (message.content === '+bots') {
-        message.reply('There are currently **3** bots in the server:\n\n*deadBot*\n*Mantaro*\n*Mee6*');
+        message.reply('There are currently **6** bots in the server:\n\n*deadBot*\n*Dyno*\n*GiveawayBot*\n*Mee6*\n*Rythm*\n*Tatsumaki*');
     }
     if (message.content === '+crystalClear') {
         message.reply('All previous chat in channel has been cleared ^~^');
@@ -21,12 +18,11 @@ client.on('message', message => {
     if (message.content === '+hehexd') {
          message.reply('T1T1T1T1T1T1T1T1T1T1');
     }
-    if (message.content === '+ask should i do my geography hw') {
-         message.reply('ofc not!!1111! geo is boring');
-    }
     if (message.content === '+annoyAdam') {
         message.reply('adam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\nadam\n\nomnomnom ehehehe');
     }
+    if (message.content === '+yasuoMain') {
+        message.reply('cancer');
 });
 
 client.login(process.env.BOT_TOKEN);
